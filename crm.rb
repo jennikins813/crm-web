85ff82cef0d906df76a9e56a5e5e6f47fb9a8335
jennikins813 <jennikins813@yahoo.com>
2014-04-01 18:27:09 -0400

    Created the form route-new_contact.erb-Step6

--- crm.rb
+++ crm.rb
@@ -2,20 +2,17 @@ require_relative 'rolodex'
 require_relative 'contact'
 require 'sinatra'
 
+@@rolodex = Rolodex.new
+
 get '/' do
 	@crm_app_name = "My CRM!"
 	erb :index
 end
 
 get "/contacts" do
-	@contacts = []
-	@contacts << Contact.new("Julie", "Hache", "julie@bitmakelabs.com", "Instructor")
-	@contacts << Contact.new("Will", "Richman", "will@bitmakelabs.com", "Co-Founder")
-	@contacts << Contact.new("Chris", "Johnston", "chris@bitmakelabs.com", "Instructor")
-	@contacts << Contact.new("Jen", "Follero", "jennikins813@yahoo.com", "Future Web-Developer")
-
 	erb :contacts
 end
 
-get '/contacts/new' do 			#use for a later step in the project
+get '/contacts/new' do 
+	erb :new_contact			
 end
